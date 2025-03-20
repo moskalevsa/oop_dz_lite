69e5d0405945d3cbee36537dc11e3a5afc5b5722
moskalevsa <moskalevsa@yandex.ru>
2025-03-20 14:58:07 +0300

    Внесение изменений

--- customer.py
+++ customer.py
@@ -1,3 +1,4 @@
+from product import order_1, order_2
 class Customer:
     """Класс, представляющий клиента.
        Атрибуы:
@@ -12,4 +13,35 @@ class Customer:
         - del_order для удаления заказов из списка заказов клиента парметр - сисок удаляемых заказов
         
     """
+    def __init__(self, name: str, orders: list):
+        self.__name = name
+        self.__orders = orders
+        
+    #свойство-геттер name
+    @property
+    def name(self):
+        return self.__name
+    
+    # свойство-сеттер
+    @name.setter
+    def name(self, name):
+        self.__code = name  
+        
+    # добавление списка товаров в заказ
+    def add_order (self, add_orders: list):
+        self.__orders.extend(add_orders)   
+    
+    #Удаление списка товаров из заказа 
+    def del_jrder (self, del_orders: list):
+        for order in del_orders:
+            if order in self.__orderss:
+                self.__poducts.remove(order)     
+    
+    # Получение списка товаров заказа
+    def get_orders(self):
+        return self.__orders    
+    
+    def __str__(self):
+        return f'Клиент:\n - полное имя клиента: -  {self.__name},\n - Количество заказов: {len(self.__orders)}'
+    
     
--- order.py
+++ order.py
@@ -1,4 +1,4 @@
-import product
+from product import car_charger, usb_type_C_charging, fm_transmitter, laptop_work_14_SSD ,bluetooth_modulator 
 class Order:
     """Класс, представляющий заказ.
        Атрибуты:
@@ -43,4 +43,36 @@ class Order:
     
     def __str__(self):
         return f'Заказ:\n - код заказа: -  {self.__code},\n - Количество товаров: {len(self.__poducts)}'
+    
+    #Список заказов
+print('СОздание заказов ------------')
+#Заказ №1
+order_1 = Order ('a+106z', [car_charger, usb_type_C_charging])
+print('Создание первого заказа ---------------------------------------')
+print(order_1)
+#Товары заказа
+print('Первый товар заказа')
+print(order_1.get_products()[0])
+#Добавление товаров в заказ
+print('Добавление товаров в заказ -----------------------------')
+order_1.add_product([fm_transmitter, laptop_work_14_SSD ])
+print(order_1)
+#Товары заказа
+print('Первый товар заказа')
+print(order_1.get_products()[0])
+#Удаление товаров из заказа
+print('Удаление товаров из заказа -----------------------------')
+order_1.del_product([car_charger])
+print(order_1)
+#Товары заказа
+print('Первый товар заказа')
+print(order_1.get_products()[0])
+
+#Заказ №2
+order_2 =Order ('a+116z', [laptop_work_14_SSD, bluetooth_modulator])
+print('Создание второго заказа ---------------------------------------')
+print(order_2)
+#Товары заказа
+print('Последний товар заказа')
+print(order_2.get_products()[-1])
     
